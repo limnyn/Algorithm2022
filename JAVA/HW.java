7a5b0c57c3c068d3e5cd3a9484de7b6905da82fc
limnyn <hogandi001@gmail.com>
2022-04-05 23:44:46 +0900

    push origin

--- JAVA/HW.java
+++ JAVA/HW.java
@@ -2,8 +2,22 @@
  * HW
  */
 public class HW {
+    
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }    
+public static void main(String[] args) {
+    String[]r = {"Diamond","Clubs","Hearts","Spades"};
+    String[]x = {"2","3","4","5","6","7","8","9","10","JACK","QUEEN","KING","ACE"};
+    int[]num = new int[5];
+    for(int i = 0; i < 5; i++){
+        num[i] = (int)(Math.random()*51);
+        for (int j = 0; j < i; j++){
+            if(num[i]==num[j]){
+                i--;
+            }
+        }
+    }
+    for (int i = 0; i < 5; i++)
+       System.out.println(r[num[i]/13]+" 의 "+x[num[i]%13]);
+
+}
 }
