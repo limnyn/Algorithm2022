72752dcce9de5e9f8045c8acd10405d26dcda48a
limnyn <hogandi001@gmail.com>
2022-05-04 10:23:28 +0900

    need dict file to engage prob1

--- py알고리즘/프로그래밍_과제2/prob1.py
+++ py알고리즘/프로그래밍_과제2/prob1.py
@@ -13,4 +13,104 @@
 #       한 번에 하나씩 순서대로 검색해야 한다.
 # (c) (b)번을 모두 수행하는데 걸린 시간을 측정하여 출력한다.
 
+import time 
 
+class dword:
+# 단어 (품사) 설명
+    word = "" #단어
+    pos = "" #품사
+    expl = "" #설명
+
+    def __init__(self, w, p, e):
+        self.word = w
+        self.pos = p
+        self.expl = e
+    
+
+
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+
+class BinarySearchTree(object):
+    def __init__(self):
+        self.root = None
+    def insert(self, data):
+        self.root = self._insert_value(self.root, data)
+        return self.root is not None
+    
+    def _insert_value(self, node, data):
+        if node is None:
+            node = Node(data)
+        else:
+            if data <= node.data:
+                node.left = self._insert_value(node.left, data)
+            else:
+                node.right = self._insert_value(node.right, data)
+        return node
+    
+    def find(self, key):
+        return self._find_value(self.root, key)
+    
+    def _find_value(self, root, key):
+        if root is None or root.data == key:
+            return root is not None
+        elif key < root.data:
+            return self._find_value(root.left, key)
+        else:
+            return self._find_value(root.right, key)
+
+    def delete(self, key):
+        self.root, deleted = self._delete_value(self.root, key)
+        return deleted
+    
+    def _delete_value(self, node, key):
+        if node is None:
+            return node, False
+    
+        deleted = False
+        if key == node.data:
+            deleted = True
+            if node.left and node.right:
+                # replace the node to the leftmost of node.right
+                parent, child = node, node.right
+                while child.left is not None:
+                    parent, child = child, child.left
+                child.left = node.left
+                if parent != node:
+                    parent.left = child.right
+                    child.right = node.right
+                node = child
+            elif node.left or node.right:
+                node = node.left or node.right
+            else:
+                node = None
+        elif key < node.data:
+            node.left, deleted = self._delete_value(node.left, key)
+        else:
+            node.right, deleted = self._delete_value(node.right, key)
+        return node, deleted
+
+
+array = [40, 4, 34, 45, 14, 55, 48, 13, 15, 49, 47]
+
+delist = [4, 14, 55, 48, 15]
+bst = BinarySearchTree()
+for x in array:
+    bst.insert(x)
+# # Find
+# print(bst.find(15)) # True
+# print(bst.find(17)) # False
+# # Delete
+# print(bst.delete(55)) # True
+# print(bst.delete(14)) # True
+# print(bst.delete(11)) # False
+start_time = time.perf_counter_ns()
+for i in delist:
+    print(i)
+    bst.delete(i)
+end_time = time.perf_counter_ns()
+
+print('코드 실행 시간: %20dns' % (end_time - start_time))
