5c4fe6248c273178c9dac7976fa06c38d7e7ae12
limnyn <hogandi001@gmail.com>
2022-05-13 23:28:32 +0900

    proex2 fin '

--- py알고리즘/프로그래밍_과제2/prob1.py
+++ py알고리즘/프로그래밍_과제2/prob1.py
@@ -1,3 +1,4 @@
+#201812177 이문현 1번
 # 1.데이터파일은 단어들이 랜덤한 순서로 배열되어 있는 영어 사전파일이다.
 # 각 라인마다 하나의 단어가 저장되어 있으며, 각 단어는 다음과 같이 형식으로 저장되어 있다.
 
@@ -5,7 +6,7 @@
 
 # 즉, 각 줄의 첫 문자열이 단어이며, 이어서 괄호 안에 그 단어의 품사가 저장되어 있고,
 # 이어서 그 단어에 대한 설명이 있다.
-# 이 사전 파일을 읽어서 이진검색트리의 각 노등 하나의 단어를 표현하는 객체가 저장되어야 한다.
+# 이 사전 파일을 읽어서 이진검색트리의 각 노드 하나의 단어를 표현하는 객체가 저장되어야 한다.
 # Python 프로그램은 다음과 같은 기능을 제공해야 한다.
 
 # (a) 프로그램을 실행하면 suffled_dict.txt 파일에 있는 모든 단어들을 순서대로 이진검색트리에 삽입한다.
@@ -13,21 +14,9 @@
 #       한 번에 하나씩 순서대로 검색해야 한다.
 # (c) (b)번을 모두 수행하는데 걸린 시간을 측정하여 출력한다.
 
+#from encodings import utf_8
 import time 
-
-class dword:
-# 단어 (품사) 설명
-    word = "" #단어
-    pos = "" #품사
-    expl = "" #설명
-
-    def __init__(self, w, p, e):
-        self.word = w
-        self.pos = p
-        self.expl = e
-    
-
-
+import re
 
 class Node(object):
     def __init__(self, data):
@@ -40,7 +29,7 @@ class BinarySearchTree(object):
     def insert(self, data):
         self.root = self._insert_value(self.root, data)
         return self.root is not None
-    
+
     def _insert_value(self, node, data):
         if node is None:
             node = Node(data)
@@ -69,12 +58,10 @@ class BinarySearchTree(object):
     def _delete_value(self, node, key):
         if node is None:
             return node, False
-    
         deleted = False
         if key == node.data:
             deleted = True
             if node.left and node.right:
-                # replace the node to the leftmost of node.right
                 parent, child = node, node.right
                 while child.left is not None:
                     parent, child = child, child.left
@@ -93,24 +80,38 @@ class BinarySearchTree(object):
             node.right, deleted = self._delete_value(node.right, key)
         return node, deleted
 
-
-array = [40, 4, 34, 45, 14, 55, 48, 13, 15, 49, 47]
-
-delist = [4, 14, 55, 48, 15]
 bst = BinarySearchTree()
-for x in array:
-    bst.insert(x)
-# # Find
-# print(bst.find(15)) # True
-# print(bst.find(17)) # False
-# # Delete
-# print(bst.delete(55)) # True
-# print(bst.delete(14)) # True
-# print(bst.delete(11)) # False
-start_time = time.perf_counter_ns()
-for i in delist:
-    print(i)
-    bst.delete(i)
-end_time = time.perf_counter_ns()
 
+
+#(a) 입력1, 각 단어 추출 후 bst에 삽입
+f = open('py알고리즘\프로그래밍_과제2\shuffled_dict.txt', 'r',encoding='utf_8')
+while True:
+    line = f.readline()
+    if not line:
+        break
+    #
+    text_filtered = re.sub('\s\(.*\n', '', line)    #각 단어는 (품사)가 나오기 전까지이다. 띄어쓰기가 포함된 단어가 있으므로 정규표현식을 사용해 단어를 구한다.
+    text_filtered.strip()
+    bst.insert(text_filtered) 
+f.close()
+#(b) 시작.
+#  입력2, 삭제할 단어 한줄씩 리스트에 삽입
+delList=[]
+start_time = time.perf_counter_ns()
+f2 = open('py알고리즘\프로그래밍_과제2\search_words.txt', 'r',encoding='utf_8')
+while True:
+    line = f2.readline()
+    if not line:
+        break
+    text_filtered = re.sub('\n', '', line)
+    text_filtered.strip()
+    delList.append(text_filtered)
+f2.close()  
+
+for delWord in delList:
+    bst.delete(delWord)
+
+#(b)종료
+end_time = time.perf_counter_ns()
 print('코드 실행 시간: %20dns' % (end_time - start_time))
+
--- py알고리즘/프로그래밍_과제2/prob2.py
+++ py알고리즘/프로그래밍_과제2/prob2.py
@@ -1,3 +1,49 @@
+#201812177이문현 2번
 # 이번에는 1번과 동일한 사전파일을 읽어서 선형적인 자료구조로 저장한다.
 # 예를 들어 배열(array), vector, arraylist, list(python) 등 각 프로그래밍 언어가 제공허는 어떤 자료구조를 이용해도 무방하다.
 # 그런 다음 1번의 (b)에서 한 일을 이 자료구조에 대해서 수행하고 걸린 시간을 측정하여 출력한다.
+
+
+import time
+import re
+
+#입력1, 각 단어 추출 후 bst에 삽입
+wordList=[]
+f = open('py알고리즘\프로그래밍_과제2\shuffled_dict.txt', 'r',encoding='utf_8')
+while True:
+    line = f.readline()
+    if not line:
+        break
+    text_filtered = re.sub('\s\(.*\n', '', line)
+    text_filtered.strip()
+    wordList.append(text_filtered) 
+f.close()
+
+#입력2, 삭제할 단어 한줄씩 리스트에 삽입
+start_time = time.perf_counter_ns()
+delList=[]
+f2 = open('py알고리즘\프로그래밍_과제2\search_words.txt', 'r',encoding='utf_8')
+while True:
+    line = f2.readline()
+    if not line:
+        break
+    text_filtered = re.sub('\n', '', line)
+    text_filtered.strip()
+    delList.append(text_filtered)
+f2.close()  
+
+#for문을 돌며 각 리스트 안의 단어들을 search해 삭제한다.
+for delWord in delList:
+    #search_words 파일에 데이터 오류가 있습니다. 
+    try:
+        wordList.remove(delWord)
+    except:
+        continue
+end_time = time.perf_counter_ns()
+
+print('코드 실행 시간: %20dns' % (end_time - start_time))
+#search_words에 데이터 오류가 있습니다. 
+#숙어(관용어)가 사전에 등재될 때 -이나 \s 등으로 여러단어가 묶여있는데 앞부분만 저장되어있습니다
+#"James's", "Spider's", "Hobson's", 'Brunswick', "Beggar's", "Lion's", 'Comet-', 'Rome', 'Lonis' 처럼
+# Lion's foot () A composite plant of the genus Prenanthes, of which several species are found in the United States.
+# 같은 경우 search_words에 Lion's로만 저장되어 있습니다.
